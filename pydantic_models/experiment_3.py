fc35ad752df5ee97f118caaafcf96b8fbb7511e5
Bekzat <tlekbay.b@gmail.com>
2025-08-30 23:23:10 +0900

    -impelemented the genral algorithm and updated the codebook construction

--- pydantic_models/experiment_3.py
+++ pydantic_models/experiment_3.py
@@ -14,30 +14,39 @@ class ClassificationSchema(BaseModel):
 
 
 
-
+### information units
 class BaseInfoSchema(BaseModel):    
     content: str = Field(..., title="The text of the information piece.")
     content_type: Literal["Greeting", "Overview", "Method", "Supplementary", "Explanation", "Description", "Conclusion", "Miscellaneous"] = Field(..., title="The type of the information piece: Greeting|Overview|Method|Supplementary|Explanation|Description|Conclusion|Miscellaneous")
     start: float = Field(..., title="The start time of the information piece. `null` if no timing metadata exists.")
     end: float = Field(..., title="The end time of the information piece. `null` if no timing metadata exists.")
 
-class InfoSchema(BaseInfoSchema):
-    procedure_segments: list[str] = Field(..., title="The list of procedure segments, e.g., ['segment1', 'segment2', ...]")
 
 class InformationPiecesSchema(BaseModel):
-  pieces: list[BaseInfoSchema] = Field(..., title="The list of information pieces classified into method, description, explanation, supplementary, or other.")
+    pieces: list[BaseInfoSchema] = Field(..., title="The list of information pieces classified into method, description, explanation, supplementary, or other.")
 
-class ProcedureSegmentsSchema(BaseModel):
-  segments: list[str] = Field(..., title="The list of procedure segments extracted from each `method` item, e.g., ['segment1', 'segment2', ...]")
 
-class StructuredPiecesSchema(BaseModel):  
-    pieces: list[InfoSchema] = Field(..., title="The list of information pieces with timing metadata.")
 
-class ContextStepSchema(BaseInfoSchema):
-    context_step: str = Field(..., title="The context step of the information piece.")
+### codebook
+
+class ItemExampleSchema(BaseModel):
+    context: str = Field(..., title="The context of the example. The context should include some text (around 10-20 words) surrounding the content and the content itself.")
+    content: str = Field(..., title="The content of the example.")
+
+class ItemSchema(BaseModel):
+    title: str = Field(..., title="The title")
+    definition: str = Field(..., title="The definition")
+    examples: list[ItemExampleSchema] = Field(..., title="The list of examples. There should be at least 1 example and no more than 3 examples.")
+
+class ItemListSchema(BaseModel):
+    items: list[ItemSchema] = Field(..., title="The list of items.")
+
+
+### labeled pieces
+class LabeledPieceSchema(BaseModel):
+    piece_id: int = Field(..., title="The provided id of the piece.")
+    label: str = Field(..., title="The label of the piece.")
 
 class LabeledPiecesSchema(BaseModel):
-    pieces: list[ContextStepSchema] = Field(..., title="The list of information pieces with context steps.")
-
-    
+    labeled_pieces: list[LabeledPieceSchema] = Field(..., title="The list of labeled pieces.")
     
